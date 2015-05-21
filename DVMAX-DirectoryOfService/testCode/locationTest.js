0acb24fac4f2b3f0d107af73bb0711374aafe82b
Larry White <lwhite@pelicaneng.com>
2015-05-21 11:17:26 -0300

    Update #6

---
+++ DVMAX-DirectoryOfService/testCode/locationTest.js
@@ -0,0 +1,10 @@
+
+var myLocation = new ds.ClinicLocation();
+
+myLocation.addressLine1 = "line1";
+myLocation.addressLine2 = "line2";
+myLocation.city = "city";
+myLocation.proviince = "province";
+myLocation.postalCode = "postalCode";
+myLocation.country = "country";
+myLocation.save();
