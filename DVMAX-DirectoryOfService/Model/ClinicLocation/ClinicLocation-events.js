af5cf6d94c8c6ad92936f1d4013dd31ab0ca1189
Larry White <lwhite@pelicaneng.com>
2015-05-22 19:24:26 -0300

    Update #12

--- DVMAX-DirectoryOfService/Model/ClinicLocation/ClinicLocation-events.js
+++ DVMAX-DirectoryOfService/Model/ClinicLocation/ClinicLocation-events.js
@@ -5,3 +5,9 @@
 //	var spacer = ' ';
 //	ClinicLocation.fullAddress = ClinicLocation.addressLine1 + spacer + ClinicLocation.addressLine2 + spacer + ClinicLocation.city + spacer + ClinicLocation.province + ClinicLocation.postalCode + ClinicLocation.country;
 //};
+
+
+model.ClinicLocation.events.save = function(event) {
+	var spacer = ' ';
+	this.fullAddress = this.addressLine1 + spacer + this.addressLine2 + spacer + this.city + spacer + this.province + this.postalCode + this.country;
+};
--- DVMAX-DirectoryOfService/Model/Reports/Reports-events.js
+++ DVMAX-DirectoryOfService/Model/Reports/Reports-events.js
@@ -4,3 +4,13 @@ model.Reports.events.init = function(event) {
 	this.date = new Date();
 	//this.version = '';
 };
+
+
+model.Reports.events.validate = function(event) {
+	// Add your code here;
+	
+	if(this.version != ClinicData.version){
+		ClinicData.version = this.version;
+	};
+	
+};
