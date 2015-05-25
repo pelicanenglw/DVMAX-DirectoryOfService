815e4beea6a44d812293b720f0d36dfe8566e0b4
Larry White <lwhite@pelicaneng.com>
2015-05-25 16:27:31 -0300

    Update #17

--- DVMAX-DirectoryOfService/WebFolder/dvmaxmetrics.waPage/scripts/index.js
+++ DVMAX-DirectoryOfService/WebFolder/dvmaxmetrics.waPage/scripts/index.js
@@ -2,16 +2,16 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
-	var button2 = {};	// @button
+	var button3 = {};	// @button
 	var Tab2GoToList = {};	// @button
 	var dataGrid5 = {};	// @dataGrid
 // @endregion// @endlock
 
 // eventHandlers// @lock
 
-	button2.click = function button2_click (event)// @startlock
+	button3.click = function button3_click (event)// @startlock
 	{// @endlock
-		waf.sources.ClinicData.query("version == :1", versionQueryString + "*");
+		waf.sources.clinicData.all;
 	};// @lock
 
 	Tab2GoToList.click = function Tab2GoToList_click (event)// @startlock
@@ -25,7 +25,7 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	};// @lock
 
 // @region eventManager// @startlock
-	WAF.addListener("button2", "click", button2.click, "WAF");
+	WAF.addListener("button3", "click", button3.click, "WAF");
 	WAF.addListener("Tab2GoToList", "click", Tab2GoToList.click, "WAF");
 	WAF.addListener("dataGrid5", "onRowDblClick", dataGrid5.onRowDblClick, "WAF");
 // @endregion
